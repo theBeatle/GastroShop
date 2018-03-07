d636eea16f0c8b18d645a81de5aa901e3244e2ac
vitalii77787 <vitalii77787@gmail.com>
2018-03-07 17:47:06 +0200

    ctor created

---
+++ SiteClient/Scripts/pages/ConstructorScript.js
@@ -0,0 +1,12 @@
+﻿$(function () {
+    $('#myTab a:last').tab('show')
+});
+$(function () {
+    $('li').click(function () {
+        var additem = $(this).clone();
+        additem.click(function () {
+            $(this).remove();
+        })
+        $('#useditems').append(additem);
+    });
+});
