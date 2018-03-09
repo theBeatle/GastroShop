1c3c20b2b89a4f07530a537c03fe41a21d414fef
DESKTOP-EPN9CP6\thebe <oleh.knyaz@gmail.com>
2018-03-09 12:42:32 +0200

    Added constructor, added href to constructor

---
+++ SiteClient/Scripts/pages/constructor.js
@@ -0,0 +1,11 @@
+﻿$(function () {
+    $('#myTab a:last').tab('show');
+
+    $('li').click(function () {
+        var additem = $(this).clone();
+        additem.click(function() {
+            $(this).remove();
+        });
+        $('#useditems').append(additem);
+    });
+});
