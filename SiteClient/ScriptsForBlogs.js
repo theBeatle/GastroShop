eab12f514882969421559e427cccb7d45200f564
Xam <@->
2018-03-11 20:17:20 +0200

    1commitinblogs

---
+++ SiteClient/ScriptsForBlogs.js
@@ -0,0 +1,35 @@
+﻿window.addEventListener('load', function () {
+
+    document.getElementById('btnShowAllCommetns').addEventListener('click', AddDivIncomments);
+
+
+});
+
+var x = 0;
+
+function AddDivIncomments()
+{
+        var createDiv = document.createElement('div');
+
+        createDiv.style.width = '90%';
+
+        createDiv.style.height = '24vh';
+
+        createDiv.style.backgroundColor = 'green';
+
+        createDiv.className = 'border';
+
+        createDiv.className = 'border-danger';
+
+        createDiv.className = 'block';
+
+        createDiv.style.margin = '0 auto';
+
+        createDiv.style.marginTop = '3%'; createDiv.style.marginBottom = '3%';
+
+        createDiv.textContent = 'text' + x;
+
+        divForShowingComments.appendChild(createDiv);
+   
+        x++;
+}
