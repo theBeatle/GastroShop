8e4f8a6276ad4e6d5be60cb71315a7ae38a73d3f
vitalii77787 <vitalii77787@gmail.com>
2018-03-12 16:42:31 +0200

    changes

--- SiteClient/Scripts/pages/ConstructorScript.js
+++ SiteClient/Scripts/pages/ConstructorScript.js
@@ -1,13 +1,20 @@
-﻿$(function () {
+﻿var value = 0;
+$(function () {
     $('#myTab a:last').tab('show')
 });
 $(function () {
     $('li').click(function () {
         var additem = $(this).clone();
+        var coast = parseInt(additem.find('div.coast').html());
+        value += coast;
         additem.click(function () {
-            $(this).remove();
+        var removedcoast = parseInt(additem.find('div.coast').html());
+        value -= removedcoast;
+        $('span.value').html(value);
+        $(this).remove();
         })
         $('#useditems').append(additem);
         $('#useditems').find('div.handle').removeClass('handle').addClass('delete');
+        $('span.value').html(value);
     });
 });
